346a92b19d49227de9d0c0f45482ffebc6cfa8cd
Srimathi Kumar <kumarsrimathi4@gmail.com>
2026-04-30 20:41:25 +0530

    first commit

---
+++ Chapter-1/js/nmethods.js
@@ -0,0 +1,4 @@
+const my_num=98;
+console.log(my_num);
+console.log(Number.isInteger(my_num));
+
---
+++ Chapter-1/js/sample.js
@@ -0,0 +1,24 @@
+const _myVariable="Mathematics";
+console.log(_myVariable);
+console.log(typeof _myVariable);
+console.log(_myVariable.length);
+console.log(_myVariable.toUpperCase());
+console.log(_myVariable.toLowerCase()); 
+console.log(_myVariable.indexOf("m"));
+console.log(_myVariable.slice(0,5));
+// console.log(_myVariable.replace("JavaScript"));
+console.log(_myVariable.split("e"));
+console.log(_myVariable.split(""));
+
+const myNumber=42;
+const myFloat=3.14;
+const myvar="42";
+console.log(myNumber);
+console.log(myFloat===myNumber);
+console.log(Number(myvar));
+console.log(parseInt(myvar)+3);
+console.log(parseInt(myvar)===myNumber);
+console.log(Number("Srimathi"+3));//Not a Number //NaN
+console.log(Number("5")+3);
+console.log(Number(true)+3);//true is 1 and false is 0
+console.log(Number(false)+3);
